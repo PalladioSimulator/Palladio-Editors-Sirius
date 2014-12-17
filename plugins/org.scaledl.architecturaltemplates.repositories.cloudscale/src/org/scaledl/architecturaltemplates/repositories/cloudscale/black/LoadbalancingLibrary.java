642929bde0d55721bfc6fd1074f5e0e148f03511
Daria Giacinto <giacinto@mail.upb.de>
2014-12-17 19:01:52 +0100

    added loadbalancing blackboxlibrary to the at repository and extended the plugin.xml

---
+++ plugins/org.scaledl.architecturaltemplates.repositories.cloudscale/src/org/scaledl/architecturaltemplates/repositories/cloudscale/black/LoadbalancingLibrary.java
@@ -0,0 +1,40 @@
+package org.scaledl.architecturaltemplates.repositories.cloudscale.black;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.modelversioning.emfprofile.Stereotype;
+import org.modelversioning.emfprofileapplication.StereotypeApplication;
+
+import de.uka.ipd.sdq.pcm.core.composition.AssemblyContext;
+import de.uka.ipd.sdq.pcm.core.entity.Entity;
+
+public class LoadbalancingLibrary {
+	
+	public LoadbalancingLibrary(){
+		super();
+	}
+	
+	public static int getNumberOfReplicas(
+			AssemblyContext assemblyContext, String taggedValueName,
+			String stereotypeName) {
+		return getValueOfIntegerTaggedValue(assemblyContext, taggedValueName, stereotypeName);	
+	}
+
+	@SuppressWarnings("unchecked")
+	private static <DATA_TYPE> DATA_TYPE getValueOfIntegerTaggedValue(
+			Entity pcmEntity, String taggedValueName,
+			String stereotypeName) {
+		EList<StereotypeApplication> pcmEntityStereotypeApplications = pcmEntity
+				.getStereotypeApplications(stereotypeName);
+		StereotypeApplication stereotypeApplication = pcmEntityStereotypeApplications
+				.get(0);
+
+		Stereotype stereotype = stereotypeApplication.getStereotype();
+
+		EStructuralFeature taggedValue = stereotype
+				.getTaggedValue(taggedValueName);
+
+		return (DATA_TYPE) stereotypeApplication.eGet(taggedValue);
+
+	}
+}
