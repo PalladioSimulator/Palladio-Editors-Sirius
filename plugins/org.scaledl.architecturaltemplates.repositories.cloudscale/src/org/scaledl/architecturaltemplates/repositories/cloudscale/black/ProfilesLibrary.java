c1f26d1ca513a55ffe6f5ccdc0ee0deaaea2627d
Daria Giacinto <giacinto@mail.upb.de>
2015-03-11 11:15:28 +0100

    ProfilesLibrary contains a method to delete model elements. DynamicHorizontalScalingAssemblyContext and AssemblyContextScaling transformation accessing the Loadbalancing and ReverseLoadbalancing transformation.

--- plugins/org.scaledl.architecturaltemplates.repositories.cloudscale/src/org/scaledl/architecturaltemplates/repositories/cloudscale/black/ProfilesLibrary.java
+++ plugins/org.scaledl.architecturaltemplates.repositories.cloudscale/src/org/scaledl/architecturaltemplates/repositories/cloudscale/black/ProfilesLibrary.java
@@ -1,14 +1,23 @@
 package org.scaledl.architecturaltemplates.repositories.cloudscale.black;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
+import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.InternalEObject;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.ecore.util.EcoreUtil.UsageCrossReferencer;
 import org.modelversioning.emfprofile.Stereotype;
 import org.modelversioning.emfprofileapplication.StereotypeApplication;
 
 import de.uka.ipd.sdq.pcm.core.entity.Entity;
+import de.uka.ipd.sdq.pcm.core.entity.NamedElement;
 
 public class ProfilesLibrary {
 
@@ -96,4 +105,43 @@ public class ProfilesLibrary {
 	        	stereotypeApplication.eSet(taggedValue, newValue);
 	        }
 	    }
+	
+	public static void delete(List<NamedElement> rootEObjects,Entity eObject) {
+          Set<EObject> eObjects = new HashSet<EObject>();
+          Set<EObject> crossResourceEObjects = new HashSet<EObject>();
+          eObjects.add(eObject);
+          for (@SuppressWarnings("unchecked")
+          TreeIterator<InternalEObject> j = (TreeIterator<InternalEObject>) (TreeIterator<?>) eObject
+                          .eAllContents(); j.hasNext();) {
+                  InternalEObject childEObject = j.next();
+                  if (childEObject.eDirectResource() != null) {
+                          crossResourceEObjects.add(childEObject);
+                  } else {
+                          eObjects.add(childEObject);
+                  }
+          }
+
+          Map<EObject, Collection<EStructuralFeature.Setting>> usages;
+         usages = UsageCrossReferencer.findAll(eObjects, rootEObjects);
+
+          for (Map.Entry<EObject, Collection<EStructuralFeature.Setting>> entry : usages
+                          .entrySet()) {
+                  EObject deletedEObject = entry.getKey();
+                  Collection<EStructuralFeature.Setting> settings = entry.getValue();
+                  for (EStructuralFeature.Setting setting : settings) {
+                          if (!eObjects.contains(setting.getEObject())
+                                          && setting.getEStructuralFeature().isChangeable()) {
+                                  EcoreUtil.remove(setting, deletedEObject);
+                          }
+                  }
+          }
+
+          EcoreUtil.remove(eObject);
+
+          for (EObject crossResourceEObject : crossResourceEObjects) {
+                  EcoreUtil.remove(crossResourceEObject.eContainer(),
+                                  crossResourceEObject.eContainmentFeature(),
+                                  crossResourceEObject);
+          }
+  }
 }
