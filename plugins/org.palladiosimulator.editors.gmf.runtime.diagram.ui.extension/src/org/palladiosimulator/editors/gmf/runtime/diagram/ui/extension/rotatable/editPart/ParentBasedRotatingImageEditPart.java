1c786ebdc5afe256e5f34b585ae1bc1325912638
Max Schettler <schettle@mail.uni-paderborn.de>
2015-03-26 18:45:43 +0100

    Added Javadoc and extracted constant

--- plugins/org.palladiosimulator.editors.gmf.runtime.diagram.ui.extension/src/org/palladiosimulator/editors/gmf/runtime/diagram/ui/extension/rotatable/editPart/ParentBasedRotatingImageEditPart.java
+++ plugins/org.palladiosimulator.editors.gmf.runtime.diagram.ui.extension/src/org/palladiosimulator/editors/gmf/runtime/diagram/ui/extension/rotatable/editPart/ParentBasedRotatingImageEditPart.java
@@ -1,29 +1,41 @@
 package org.palladiosimulator.editors.gmf.runtime.diagram.ui.extension.rotatable.editPart;
 
-import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.geometry.Rectangle;
-import org.eclipse.gef.GraphicalEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.ShapeEditPart;
 import org.eclipse.gmf.runtime.notation.View;
 
 /**
+ * An {@link AbstractRotatableImageEditPart} that rotates the image based on the
+ * position relative to its parent. This is meant to be used only as a style of
+ * Sirius` BorderedNodes.
  * 
  * @author max
  *
  */
 public class ParentBasedRotatingImageEditPart extends
 		AbstractRotatableImageEditPart {
-	
-	private enum Orientation {
-		TOP,
-		BOTTOM,
-		LEFT,
-		RIGHT
+
+	/**
+	 * Contains the offset the BorderedNode`s top-left corner is offset into the
+	 * parent. This is used for the compution in
+	 * {@link #orientationRelativeToEditPart(ShapeEditPart)}.
+	 */
+	private static final int CHILD_POSITION_OFFSET = 10;
+
+	/**
+	 * An enum that codes the orientation of the image or the side the BorderedNode is attached to.
+	 * @author max
+	 *
+	 */
+	private static enum Orientation {
+		TOP, BOTTOM, LEFT, RIGHT
 	}
 
 	/**
 	 * Default constructor.
-	 * @param view view
+	 * 
+	 * @param view
+	 *            view
 	 */
 	public ParentBasedRotatingImageEditPart(View view) {
 		super(view);
@@ -37,7 +49,8 @@ public class ParentBasedRotatingImageEditPart extends
 		if (!(getParent() instanceof ShapeEditPart))
 			return;
 
-		switch (orientationRelativeToEditPart((ShapeEditPart) getParent().getParent())) {
+		switch (orientationRelativeToEditPart((ShapeEditPart) getParent()
+				.getParent())) {
 		case TOP:
 			setFigureAtTop();
 			break;
@@ -55,19 +68,22 @@ public class ParentBasedRotatingImageEditPart extends
 
 	/**
 	 * Determines on which side of its parent this EditPart is displayed.
-	 * @param parent the parent EditPart
+	 * 
+	 * @param parent
+	 *            the parent EditPart
 	 * @return the orientation
 	 */
 	private Orientation orientationRelativeToEditPart(ShapeEditPart parent) {
 		Rectangle parentRectangle = parent.getFigure().getBounds();
-		System.out.println("ParentRectangle: " + parentRectangle + " this location: " + getLocation());
+		System.out.println("ParentRectangle: " + parentRectangle
+				+ " this location: " + getLocation());
 		if (parentRectangle.getTop().y > getLocation().y) {
 			return Orientation.TOP;
-		} else if (parentRectangle.getBottom().y - 10 == getLocation().y) {
+		} else if (parentRectangle.getBottom().y - CHILD_POSITION_OFFSET == getLocation().y) {
 			return Orientation.BOTTOM;
 		} else if (parentRectangle.getLeft().x > getLocation().x) {
 			return Orientation.LEFT;
-		} else if (parentRectangle.getRight().x - 10 == getLocation().x) {
+		} else if (parentRectangle.getRight().x - CHILD_POSITION_OFFSET == getLocation().x) {
 			return Orientation.RIGHT;
 		}
 
