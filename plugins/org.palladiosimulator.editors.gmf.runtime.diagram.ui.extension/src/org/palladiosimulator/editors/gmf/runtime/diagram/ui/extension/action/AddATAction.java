0ed63086ec401880f90594228a61cbbda8456765
Max Schettler <schettle@mail.uni-paderborn.de>
2015-04-16 19:04:32 +0200

    Commented AddATAction

--- plugins/org.palladiosimulator.editors.gmf.runtime.diagram.ui.extension/src/org/palladiosimulator/editors/gmf/runtime/diagram/ui/extension/action/AddATAction.java
+++ plugins/org.palladiosimulator.editors.gmf.runtime.diagram.ui.extension/src/org/palladiosimulator/editors/gmf/runtime/diagram/ui/extension/action/AddATAction.java
@@ -9,14 +9,16 @@ import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.sirius.business.api.session.SessionManager;
 import org.eclipse.sirius.tools.api.ui.IExternalJavaAction;
 import org.eclipse.ui.PlatformUI;
+import org.modelversioning.emfprofile.Stereotype;
 import org.palladiosimulator.editors.util.dialogs.ArchitecturalTemplateSelectEObjectDialog;
 import org.scaledl.architecturaltemplates.type.AT;
 
 import de.uka.ipd.sdq.pcm.system.System;
 
 /**
- * A Sirius action that is used to add an ArchitecturalTemplate Role to the {@link System} it is applied to.
- * Executing the action will create an dialog asking the user to select an {@link AT}.
+ * A Sirius action that is used to add an ArchitecturalTemplate Role to the
+ * {@link System} it is applied to. Executing the action will create an dialog
+ * asking the user to select an {@link AT}.
  * 
  * FIXME: use org.eclipse.sirius.tools.api.ui.IExternalJavaAction2
  * 
@@ -25,8 +27,17 @@ import de.uka.ipd.sdq.pcm.system.System;
  */
 public class AddATAction implements IExternalJavaAction {
 
+	/**
+	 * The name of the parameter to be given. This is an arbitrary object from
+	 * the editor to get the corresponding {@link Session}.
+	 */
 	private static final Object SELF_KEY = "self";
 
+	/**
+	 * Creates an {@link ArchitecturalTemplateSelectEObjectDialog} that queries
+	 * the user for an {@link AT} and applies the corresponding
+	 * {@link Stereotype} to the selected {@link System}.
+	 */
 	@Override
 	public void execute(Collection<? extends EObject> selections,
 			Map<String, Object> parameters) {
@@ -34,12 +45,13 @@ public class AddATAction implements IExternalJavaAction {
 		if (parameter == null || !(parameter instanceof EObject)) {
 			return;
 		}
-		final ResourceSet resourceSet = SessionManager.INSTANCE.getSession(
-				(EObject) parameter).getTransactionalEditingDomain().getResourceSet();
+		final ResourceSet resourceSet = SessionManager.INSTANCE
+				.getSession((EObject) parameter)
+				.getTransactionalEditingDomain().getResourceSet();
 
 		final ArchitecturalTemplateSelectEObjectDialog atSelectionDialog = new ArchitecturalTemplateSelectEObjectDialog(
 				PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
-				resourceSet, ArchitecturalTemplateSelectEObjectDialog .Type.AT);
+				resourceSet, ArchitecturalTemplateSelectEObjectDialog.Type.AT);
 		if (atSelectionDialog.open() == Dialog.OK) {
 			for (EObject object : selections) {
 				java.lang.System.out.println(object);
@@ -47,6 +59,9 @@ public class AddATAction implements IExternalJavaAction {
 		}
 	}
 
+	/**
+	 * Checks whether there is only one selection and if its a {@link System}.
+	 */
 	@Override
 	public boolean canExecute(Collection<? extends EObject> selections) {
 		if (selections.size() != 1) {
