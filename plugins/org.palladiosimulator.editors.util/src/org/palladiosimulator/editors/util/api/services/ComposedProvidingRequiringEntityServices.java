3060866a5676c43d5341535c9d0883cb04b103ee
Max Schettler <max.schettler@posteo.de>
2015-04-28 17:24:29 +0200

    Migrated Add AT Action to the palette

--- plugins/org.palladiosimulator.editors.util/src/org/palladiosimulator/editors/util/api/services/ComposedProvidingRequiringEntityServices.java
+++ plugins/org.palladiosimulator.editors.util/src/org/palladiosimulator/editors/util/api/services/ComposedProvidingRequiringEntityServices.java
@@ -4,7 +4,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.antlr.runtime.ANTLRStringStream;
