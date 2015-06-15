65e983a22c02c1f73fea4e8d7581893f3e5a59bc
Max Schettler <max.schettler@posteo.de>
2015-06-15 21:28:54 +0200

    Fixed role-node-inset and refactored the rotating-node EditPart

---
+++ plugins/org.palladiosimulator.editors.gmf.runtime.diagram.ui.extension/src/org/palladiosimulator/editors/gmf/runtime/diagram/ui/extension/ComposedProvidingRequiringEntityEditorConstants.java
@@ -0,0 +1,10 @@
+package org.palladiosimulator.editors.gmf.runtime.diagram.ui.extension;
+
+public final class ComposedProvidingRequiringEntityEditorConstants {
+
+	public static final Object COMPOSED_PROVIDING_REQUIRING_ENTITY_MAPPING = "ComposedProvidingRequiringEntity Node";
+	public static final Object ASSEMBLY_CONTEXT_MAPPING = "AssemblyContext Container";
+
+	private ComposedProvidingRequiringEntityEditorConstants() {
+	}
+}
---
+++ plugins/org.palladiosimulator.editors.gmf.runtime.diagram.ui.extension/src/org/palladiosimulator/editors/gmf/runtime/diagram/ui/extension/styleconfiguration/ComposedProvidingRequringEntityStyleConfiguration.java
@@ -0,0 +1,36 @@
+package org.palladiosimulator.editors.gmf.runtime.diagram.ui.extension.styleconfiguration;
+
+import java.util.function.Predicate;
+
+import org.eclipse.sirius.diagram.description.DiagramElementMapping;
+import org.eclipse.sirius.diagram.ui.tools.api.graphical.edit.styles.IStyleConfigurationProvider;
+import org.eclipse.sirius.diagram.ui.tools.api.graphical.edit.styles.StyleConfiguration;
+import org.eclipse.sirius.viewpoint.Style;
+import org.palladiosimulator.editors.gmf.runtime.diagram.ui.extension.ComposedProvidingRequiringEntityEditorConstants;
+
+public class ComposedProvidingRequringEntityStyleConfiguration implements
+		IStyleConfigurationProvider {
+
+	private static final Predicate<DiagramElementMapping> isRoleParentNode = mapping -> mapping
+			.getName()
+			.equals(ComposedProvidingRequiringEntityEditorConstants.ASSEMBLY_CONTEXT_MAPPING)
+			|| mapping
+					.getName()
+					.equals(ComposedProvidingRequiringEntityEditorConstants.COMPOSED_PROVIDING_REQUIRING_ENTITY_MAPPING);
+
+	public ComposedProvidingRequringEntityStyleConfiguration() {}
+
+	@Override
+	public boolean provides(DiagramElementMapping mapping, Style style) {
+		return isRoleParentNode.test(mapping);
+	}
+
+	@Override
+	public StyleConfiguration createStyleConfiguration(
+			DiagramElementMapping mapping, Style style) {
+		if (isRoleParentNode.test(mapping)) {
+			return new RoleParentStyleConfiguration();
+		}
+		return null;
+	}
+}
