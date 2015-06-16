1ffee8db39b6ca63fd1e3d832c8e9dab74ade651
Max Schettler <max.schettler@posteo.de>
2015-06-16 13:59:07 +0200

    Updated documentation

--- plugins/org.palladiosimulator.editors.composedprovidingrequiringentity.design/documentation/TODO.md
+++ plugins/org.palladiosimulator.editors.composedprovidingrequiringentity.design/documentation/TODO.md
@@ -4,7 +4,6 @@ Tasks:
 
 Known Bugs:
 - .odesign does not validate
-- Role-images overlap with AssemblyContext Container
 - Support for filters on EventChannelSinkConnectors
 - Prevent creation of invalid edges (-> preconditions)
 - Sirius Bug (fixed upstream): Preconditions not working. Results in wrong behavior with instantiating VariableUsages and editing VariableCharacterisations
--- plugins/org.palladiosimulator.editors.gmf.runtime.diagram.ui.extension/src/org/palladiosimulator/editors/gmf/runtime/diagram/ui/extension/borderItemLocator/RoleBorderItemLocator.java
+++ plugins/org.palladiosimulator.editors.gmf.runtime.diagram.ui.extension/src/org/palladiosimulator/editors/gmf/runtime/diagram/ui/extension/borderItemLocator/RoleBorderItemLocator.java
@@ -5,15 +5,15 @@ import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.sirius.diagram.ui.tools.api.figure.locator.DBorderItemLocator;
 
 /**
- * This class is a bad workaround for a Sirius bug and should be removed as soon
- * as possible. It prevents the border item offset to be set to the default
- * value by overriding the {@link #getBorderItemOffset()}-method.
+ * This class is a workaround for unexpected Sirius behavior and should be
+ * removed as soon as possible. It prevents the border item offset to be set to
+ * the default value by overriding the {@link #setBorderItemOffset(Dimension)}-method.
  * 
  * @author Max Schettler
  *
  */
 public class RoleBorderItemLocator extends DBorderItemLocator {
-	
+
 	private static final Dimension BORDER_ITEM_OFFSET = new Dimension(0, 0);
 
 	public RoleBorderItemLocator(IFigure parentFigure, int preferredSide) {
@@ -25,6 +25,5 @@ public class RoleBorderItemLocator extends DBorderItemLocator {
 	public void setBorderItemOffset(final Dimension borderItemOffset) {
 		// Prevent resetting the borderItemOffset
 	}
-	
 
 }
