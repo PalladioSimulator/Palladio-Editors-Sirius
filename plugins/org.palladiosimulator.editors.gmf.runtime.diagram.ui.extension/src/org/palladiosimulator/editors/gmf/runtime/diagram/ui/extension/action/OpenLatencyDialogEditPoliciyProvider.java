9edbee6e185496495dc7c10a60ad9c405352f080
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2015-07-01 14:42:06 +0200

    Added edit policy provider for the latency dialog

---
+++ plugins/org.palladiosimulator.editors.gmf.runtime.diagram.ui.extension/src/org/palladiosimulator/editors/gmf/runtime/diagram/ui/extension/action/OpenLatencyDialogEditPoliciyProvider.java
@@ -0,0 +1,20 @@
+package org.palladiosimulator.editors.gmf.runtime.diagram.ui.extension.action;
+
+import org.eclipse.gef.EditPart;
+import org.eclipse.gmf.runtime.common.core.service.IOperation;
+
+public class OpenLatencyDialogEditPoliciyProvider extends AbstractEditPolicyProvider {
+
+    @Override
+    public void createEditPolicies(final EditPart editPart) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean provides(final IOperation operation) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
