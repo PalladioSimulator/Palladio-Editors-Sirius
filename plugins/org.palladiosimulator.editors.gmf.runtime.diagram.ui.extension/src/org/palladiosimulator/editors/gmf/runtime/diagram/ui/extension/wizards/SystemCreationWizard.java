2effbde818834bae3e8f8c3d5b657625fe0db0d1
Max Schettler <schettle@mail.upb.de>
2015-07-28 16:30:06 +0200

    CPREE: Add empty system model wizard

---
+++ plugins/org.palladiosimulator.editors.gmf.runtime.diagram.ui.extension/src/org/palladiosimulator/editors/gmf/runtime/diagram/ui/extension/wizards/SystemCreationWizard.java
@@ -0,0 +1,22 @@
+package org.palladiosimulator.editors.gmf.runtime.diagram.ui.extension.wizards;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchWizard;
+
+public class SystemCreationWizard extends Wizard implements IWorkbenchWizard {
+
+	@Override
+	public boolean performFinish() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
