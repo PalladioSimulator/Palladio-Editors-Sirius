11bb4db82a22a8bcb6f60ed6bd1f24ef68a4e02d
devilmay <edith.kegel@gmail.com>
2015-07-28 17:02:00 +0200

    addlinkingresource updated

--- plugins/org.palladiosimulator.editors.gmf.runtime.diagram.ui.extension/src/org/palladiosimulator/editors/gmf/runtime/diagram/ui/extension/action/AddLinkingResourceAction.java
+++ plugins/org.palladiosimulator.editors.gmf.runtime.diagram.ui.extension/src/org/palladiosimulator/editors/gmf/runtime/diagram/ui/extension/action/AddLinkingResourceAction.java
@@ -1,53 +1,116 @@
 package org.palladiosimulator.editors.gmf.runtime.diagram.ui.extension.action;
 
-import org.eclipse.gef.EditPart;
-import org.eclipse.gmf.runtime.common.core.service.IOperation;
-import org.eclipse.gmf.runtime.common.core.service.IProviderChangeListener;
-import org.eclipse.gmf.runtime.diagram.ui.services.editpolicy.IEditPolicyProvider;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Map;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gmf.runtime.common.core.command.CommandResult;
+import org.eclipse.gmf.runtime.common.core.command.ICommand;
+import org.eclipse.gmf.runtime.emf.type.core.IElementType;
+import org.eclipse.gmf.runtime.emf.type.core.commands.EditElementCommand;
+import org.eclipse.gmf.runtime.emf.type.core.requests.ConfigureRequest;
+import org.eclipse.gmf.runtime.emf.type.core.requests.CreateElementRequest;
+import org.eclipse.gmf.runtime.emf.type.core.requests.IEditCommandRequest;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.sirius.tools.api.ui.IExternalJavaAction;
+import org.eclipse.ui.PlatformUI;
+import org.modelversioning.emfprofile.Stereotype;
+import org.palladiosimulator.mdsdprofiles.api.StereotypeAPI;
+import org.palladiosimulator.pcm.resourceenvironment.LinkingResource;
+import org.palladiosimulator.pcm.resourceenvironment.ResourceEnvironment;
+import org.palladiosimulator.pcm.resourceenvironment.ResourceenvironmentFactory;
+import org.palladiosimulator.pcm.system.System;
+import org.scaledl.architecturaltemplates.api.ArchitecturalTemplateAPI;
+import org.scaledl.architecturaltemplates.type.AT;
+import org.scaledl.architecturaltemplates.ui.dialogs.ArchitecturalTemplateSelectionDialog;
+import org.scaledl.architecturaltemplates.ui.dialogs.RoleStereotypeSelectionDialog;
 
 
-public class AddLinkingResourceAction implements  IEditPolicyProvider  {
+public class AddLinkingResourceAction extends EditElementCommand implements  IExternalJavaAction  {
 
-	/*public AddLinkingResourceAction(){
-		super(PalladioComponentModelElementTypes.LinkingResource_2005);
+
+	private static final String DIALOG_MESSAGE = "Add a Linking Resource";
+
+	
+	public AddLinkingResourceAction(CreateElementRequest req){
+		super(req.getLabel(),null,req);
+	}
+	
+	public boolean getElementToEdit(Collection<? extends EObject> selections){
+	//	EObject container = ((CreateElementRequest) getRequest()).getContainer();
+		for (EObject object : selections) {
+			return object instanceof LinkingResource;
 		}
+		return true;
+	}
+	
+	
 	@Override
-	public boolean canExecute(Collection<? extends EObject> arg0) {
-		// TODO Auto-generated method stub
+	public boolean canExecute(Collection<? extends EObject> selections) {
+		/*if (selections.size() != 1) {
+			return false;
+		}
+		for (EObject object : selections) {
+			return object instanceof LinkingResource;
+		}*/
+		
 		return true;
 	}
 
+
 	@Override
-	public void execute(Collection<? extends EObject> arg0, Map<String, Object> arg1) {
-		// TODO Auto-generated method stub
-		OpenLatencyDialog openLatencyDialog = new OpenLatencyDialog();
-		//installEditPolicy <- openLatencyDialog ist das? 
+	protected CommandResult doExecuteWithResult(IProgressMonitor monitor, IAdaptable info) throws ExecutionException {
+		LinkingResource linkingResource = ResourceenvironmentFactory.eINSTANCE.createLinkingResource();
+
+		ResourceEnvironment owner = (ResourceEnvironment) getElementToEdit();
+		owner.getLinkingResources__ResourceEnvironment().add(linkingResource);
 		
-		System.out.println("huhu");
+		doConfigure(linkingResource, monitor, info);
+		return CommandResult.newOKCommandResult(linkingResource);
 	}
-*/
+	
+	
+	protected void doConfigure(LinkingResource linkingResource, IProgressMonitor monitor, IAdaptable info)
+            throws ExecutionException {
+        IElementType elementType = ((CreateElementRequest) getRequest()).getElementType();
+        ConfigureRequest configureRequest = new ConfigureRequest(getEditingDomain(), linkingResource, elementType);
+        configureRequest.setClientContext(((CreateElementRequest) getRequest()).getClientContext());
+        configureRequest.addParameters(getRequest().getParameters());
+        ICommand configureCommand = elementType.getEditCommand(configureRequest);
+        if (configureCommand != null && configureCommand.canExecute()) {
+            configureCommand.execute(monitor, info);
+        }
+    }
 	@Override
-	public void addProviderChangeListener(IProviderChangeListener listener) {
-		// TODO Auto-generated method stub
+	public void execute(Collection<? extends EObject> selections, Map<String, Object> parameter) {
+		//final LinkingResource linkingResource = (LinkingResource) selections.iterator().next();
+		//LinkingResource linkingResource = ResourceenvironmentFactory.eINSTANCE.createLinkingResource();
+
+		//ResourceEnvironment owner = (ResourceEnvironment) getElementToEdit()
+	  
+		/*ArchitecturalTemplateSelectionDialog profileSelectionDialog = new ArchitecturalTemplateSelectionDialog(
+				PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell());
+
+		profileSelectionDialog.setElements((AT[]) ArchitecturalTemplateAPI.getRegisteredArchitecturalTemplates()
+				.toArray(new AT[ArchitecturalTemplateAPI.getRegisteredArchitecturalTemplates().size()]));
+		profileSelectionDialog.setMessage(DIALOG_MESSAGE);
+
+		if (profileSelectionDialog.open() != Dialog.OK) {
+			return;
+		}
+
+		final AT at = profileSelectionDialog.getResultArchitecturalTemplate();
+
+		//ArchitecturalTemplateAPI.applyArchitecturalTemplate(linkingResource, at);*/
 		
 	}
 
-	@Override
-	public boolean provides(IOperation operation) {
-		// TODO Auto-generated method stub
-		return false;
-	}
 
-	@Override
-	public void removeProviderChangeListener(IProviderChangeListener listener) {
-		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	public void createEditPolicies(EditPart editPart) {
-		// TODO Auto-generated method stub
-		
-	}
+	
 
 }
