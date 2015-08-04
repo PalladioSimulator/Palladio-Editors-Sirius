a248a3cd07aba5f6cfbff593b75ef327aec1dfb0
devilmay <edith.kegel@gmail.com>
2015-08-04 14:36:36 +0200

    addLinking Resource improved , insert addlatencyThroughputcommand

--- plugins/org.palladiosimulator.editors.gmf.runtime.diagram.ui.extension/src/org/palladiosimulator/editors/gmf/runtime/diagram/ui/extension/action/AddLinkingResourceAction.java
+++ plugins/org.palladiosimulator.editors.gmf.runtime.diagram.ui.extension/src/org/palladiosimulator/editors/gmf/runtime/diagram/ui/extension/action/AddLinkingResourceAction.java
@@ -1,111 +1,96 @@
 package org.palladiosimulator.editors.gmf.runtime.diagram.ui.extension.action;
 
 import java.util.Collection;
-import java.util.LinkedList;
 import java.util.Map;
 
-import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.runtime.IAdaptable;
-import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.common.command.Command;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.gmf.runtime.common.core.command.CommandResult;
-import org.eclipse.gmf.runtime.common.core.command.ICommand;
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.gmf.runtime.common.core.command.CompositeCommand;
+import org.eclipse.gmf.runtime.emf.type.core.ElementTypeRegistry;
 import org.eclipse.gmf.runtime.emf.type.core.IElementType;
-import org.eclipse.gmf.runtime.emf.type.core.commands.EditElementCommand;
-import org.eclipse.gmf.runtime.emf.type.core.requests.ConfigureRequest;
+import org.eclipse.gmf.runtime.emf.type.core.commands.CreateElementCommand;
 import org.eclipse.gmf.runtime.emf.type.core.requests.CreateElementRequest;
-import org.eclipse.gmf.runtime.emf.type.core.requests.IEditCommandRequest;
-import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.sirius.business.api.session.Session;
+import org.eclipse.sirius.business.api.session.SessionManager;
 import org.eclipse.sirius.tools.api.ui.IExternalJavaAction;
-import org.eclipse.ui.PlatformUI;
-import org.modelversioning.emfprofile.Stereotype;
-import org.palladiosimulator.mdsdprofiles.api.StereotypeAPI;
+import org.palladiosimulator.editors.gmf.runtime.diagram.ui.extension.commands.AddLatencyAndThroughputRecordingCommand;
+import org.palladiosimulator.pcm.resourceenvironment.CommunicationLinkResourceSpecification;
 import org.palladiosimulator.pcm.resourceenvironment.LinkingResource;
 import org.palladiosimulator.pcm.resourceenvironment.ResourceEnvironment;
 import org.palladiosimulator.pcm.resourceenvironment.ResourceenvironmentFactory;
-import org.palladiosimulator.pcm.system.System;
-import org.scaledl.architecturaltemplates.api.ArchitecturalTemplateAPI;
-import org.scaledl.architecturaltemplates.type.AT;
-import org.scaledl.architecturaltemplates.ui.dialogs.ArchitecturalTemplateSelectionDialog;
-import org.scaledl.architecturaltemplates.ui.dialogs.RoleStereotypeSelectionDialog;
+import org.palladiosimulator.pcm.resourceenvironment.ResourceenvironmentPackage;
 
 
-public class AddLinkingResourceAction extends EditElementCommand implements  IExternalJavaAction  {
+public class AddLinkingResourceAction implements  IExternalJavaAction  {
 
 
 	private static final String DIALOG_MESSAGE = "Add a Linking Resource";
+	private static final String RESOURCE_COMMUNICATION_LINK_RESOURCE_SPECIFICATION = "org.palladiosimulator.pcm.resourceenvironment.CommunicationLinkResourceSpecification";
 
-	
-	public AddLinkingResourceAction(CreateElementRequest req){
-		super(req.getLabel(),null,req);
+	public AddLinkingResourceAction(){
+		super();
 	}
-	
-	public boolean getElementToEdit(Collection<? extends EObject> selections){
-	//	EObject container = ((CreateElementRequest) getRequest()).getContainer();
-		for (EObject object : selections) {
-			return object instanceof LinkingResource;
-		}
-		return true;
-	}
-	
+
 	
 	@Override
 	public boolean canExecute(Collection<? extends EObject> selections) {
-		/*if (selections.size() != 1) {
-			return false;
-		}
 		for (EObject object : selections) {
-			return object instanceof LinkingResource;
-		}*/
-		
-		return true;
-	}
-
-
-	@Override
-	protected CommandResult doExecuteWithResult(IProgressMonitor monitor, IAdaptable info) throws ExecutionException {
-		LinkingResource linkingResource = ResourceenvironmentFactory.eINSTANCE.createLinkingResource();
-
-		ResourceEnvironment owner = (ResourceEnvironment) getElementToEdit();
-		owner.getLinkingResources__ResourceEnvironment().add(linkingResource);
-		
-		doConfigure(linkingResource, monitor, info);
-		return CommandResult.newOKCommandResult(linkingResource);
-	}
-	
-	
-	protected void doConfigure(LinkingResource linkingResource, IProgressMonitor monitor, IAdaptable info)
-            throws ExecutionException {
-        IElementType elementType = ((CreateElementRequest) getRequest()).getElementType();
-        ConfigureRequest configureRequest = new ConfigureRequest(getEditingDomain(), linkingResource, elementType);
-        configureRequest.setClientContext(((CreateElementRequest) getRequest()).getClientContext());
-        configureRequest.addParameters(getRequest().getParameters());
-        ICommand configureCommand = elementType.getEditCommand(configureRequest);
-        if (configureCommand != null && configureCommand.canExecute()) {
-            configureCommand.execute(monitor, info);
-        }
-    }
-	@Override
-	public void execute(Collection<? extends EObject> selections, Map<String, Object> parameter) {
-		//final LinkingResource linkingResource = (LinkingResource) selections.iterator().next();
-		//LinkingResource linkingResource = ResourceenvironmentFactory.eINSTANCE.createLinkingResource();
-
-		//ResourceEnvironment owner = (ResourceEnvironment) getElementToEdit()
-	  
-		/*ArchitecturalTemplateSelectionDialog profileSelectionDialog = new ArchitecturalTemplateSelectionDialog(
-				PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell());
-
-		profileSelectionDialog.setElements((AT[]) ArchitecturalTemplateAPI.getRegisteredArchitecturalTemplates()
-				.toArray(new AT[ArchitecturalTemplateAPI.getRegisteredArchitecturalTemplates().size()]));
-		profileSelectionDialog.setMessage(DIALOG_MESSAGE);
-
-		if (profileSelectionDialog.open() != Dialog.OK) {
-			return;
+			if( object instanceof ResourceEnvironment)
+				return true;
 		}
+		return false;
+	}
+	
+	@Override
+	public void execute(Collection<? extends EObject> selections, Map<String, Object> parameters) {
+		java.lang.System.out.println("actual execute befehl");
+		
+		final ResourceEnvironment resourceEnvironment = (ResourceEnvironment) selections.iterator().next();
+      
+		
+		/* IElementType elementType = ElementTypeRegistry.getInstance().getType( RESOURCE_COMMUNICATION_LINK_RESOURCE_SPECIFICATION);
+        
 
-		final AT at = profileSelectionDialog.getResultArchitecturalTemplate();
+        CreateElementRequest createElementRequest = new CreateElementRequest(resourceEnvironment,elementType);
+        createElementRequest.setContainmentFeature(ResourceenvironmentPackage.eINSTANCE
+                .getLinkingResource_CommunicationLinkResourceSpecifications_LinkingResource());
+        CreateElementCommand createElementCommand = new CreateElementCommand(createElementRequest);
 
-		//ArchitecturalTemplateAPI.applyArchitecturalTemplate(linkingResource, at);*/
+       
+
+        CompositeCommand compositeCommand = new CompositeCommand("");
+        compositeCommand.add(createElementCommand);
+        //compositeCommand.add(new SetLatencyThroughputAndLanTypeCommand(request));
+*/
+		
+		final Object parameter = parameters.get("newLinkingResource");
+	        if (parameter == null || !(parameter instanceof CommunicationLinkResourceSpecification)) {
+	        	return;
+	        }
+			//final EditingDomain edomain = SessionManager.INSTANCE.getSession((EObject) parameter).getTransactionalEditingDomain();
+			//new org.eclipse.emf.edit.ui.action.LoadResourceAction(edomain).run();
+    	final TransactionalEditingDomain domain = SessionManager.INSTANCE.getSession((EObject)parameter).getTransactionalEditingDomain();
+    	
+    	
+    	
+    	//CommunicationLinkRes mit Factory erstellen
+    	//CommunicationLinkResourceSpecification communicationLinkingResource = ResourceenvironmentFactory.eINSTANCE.createCommunicationLinkResourceSpecification();
+    	
+    	//communicationLinkingResource befüllen...
+    	//TODO
+    	
+    	//oder von sirius erstellte LR nutzen ?
+    	//CommunicationLinkResourceSpecification communicationLinkingResource = (CommunicationLinkResourceSpecification) parameters.get("newLinkingResource");
+		
+    	AddLatencyAndThroughputRecordingCommand acommand = new AddLatencyAndThroughputRecordingCommand(domain,(CommunicationLinkResourceSpecification) parameter);
+		
+		domain.getCommandStack().execute(acommand);
+    	
+
+
+         	
 		
 	}
 
