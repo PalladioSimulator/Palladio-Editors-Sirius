c37790fc4a7bad2923c8f36f41c3b2a9e76ef533
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2015-11-04 11:08:06 +0100

    Fixed bad class cast, making the code system-specific

--- plugins/org.palladiosimulator.editors.gmf.runtime.diagram.ui.extension/src/org/palladiosimulator/editors/gmf/runtime/diagram/ui/extension/action/ApplyUnapplyProfileAction.java
+++ plugins/org.palladiosimulator.editors.gmf.runtime.diagram.ui.extension/src/org/palladiosimulator/editors/gmf/runtime/diagram/ui/extension/action/ApplyUnapplyProfileAction.java
@@ -10,53 +10,51 @@ import org.eclipse.emf.edit.ui.celleditor.FeatureEditorDialog;
 import org.eclipse.emf.edit.ui.provider.AdapterFactoryLabelProvider;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.viewers.ILabelProvider;
-import org.eclipse.sirius.diagram.DDiagramElement;
 import org.eclipse.sirius.tools.api.ui.IExternalJavaAction;
+import org.eclipse.sirius.viewpoint.DSemanticDecorator;
 import org.eclipse.ui.PlatformUI;
 import org.modelversioning.emfprofile.Profile;
 import org.modelversioning.emfprofile.provider.EMFProfileItemProviderAdapterFactory;
 import org.palladiosimulator.mdsdprofiles.api.ProfileAPI;
 
 /**
- * A sirius action for selecting and applying a {@link Profile} to the
- * {@link Resource} of the selected {@link EObject}. selected object.
+ * A sirius action for selecting and applying a {@link Profile} to the {@link Resource} of the
+ * selected {@link EObject}. selected object.
  * 
  * @author Max Schettler
  *
  */
 public class ApplyUnapplyProfileAction implements IExternalJavaAction {
 
-	private static final ILabelProvider LABEL_PROVIDER = getLabelProvider();
-	private static final String SELECT_APPLIED_PROFILES_MESSAGE = "Select applied profiles";
+    private static final ILabelProvider LABEL_PROVIDER = getLabelProvider();
+    private static final String SELECT_APPLIED_PROFILES_MESSAGE = "Select applied profiles";
 
-	@Override
-	public boolean canExecute(Collection<? extends EObject> selections) {
-		return selections.size() == 1;
-	}
+    @Override
+    public boolean canExecute(final Collection<? extends EObject> selections) {
+        return selections.size() == 1;
+    }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public void execute(Collection<? extends EObject> selections, Map<String, Object> parameters) {
-		final EObject target = ((DDiagramElement) selections.iterator().next()).getTarget();
-		final Resource targetResource = target.eResource();
+    @SuppressWarnings("unchecked")
+    @Override
+    public void execute(final Collection<? extends EObject> selections, final Map<String, Object> parameters) {
+        final EObject target = ((DSemanticDecorator) selections.iterator().next()).getTarget();
+        final Resource targetResource = target.eResource();
 
-		final FeatureEditorDialog profileSelectionDialog = new FeatureEditorDialog(
-				PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), LABEL_PROVIDER, 
-				target, target.eClass().getEAllStructuralFeatures().get(0).getEType(),
-				ProfileAPI.getAppliedProfiles(targetResource), 
-				SELECT_APPLIED_PROFILES_MESSAGE,
-				ProfileAPI.getApplicableProfiles(targetResource), 
-				false, true, true);
+        final FeatureEditorDialog profileSelectionDialog = new FeatureEditorDialog(
+                PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), LABEL_PROVIDER, target,
+                target.eClass().getEAllStructuralFeatures().get(0).getEType(),
+                ProfileAPI.getAppliedProfiles(targetResource), SELECT_APPLIED_PROFILES_MESSAGE,
+                ProfileAPI.getApplicableProfiles(targetResource), false, true, true);
 
-		if (profileSelectionDialog.open() == Dialog.OK) {
-			ProfileAPI.updateProfileApplications(targetResource, (EList<Profile>) profileSelectionDialog.getResult());
-		}
+        if (profileSelectionDialog.open() == Dialog.OK) {
+            ProfileAPI.updateProfileApplications(targetResource, (EList<Profile>) profileSelectionDialog.getResult());
+        }
 
-	}
+    }
 
-	private static ILabelProvider getLabelProvider() {
-		final EMFProfileItemProviderAdapterFactory adapterFactory = new EMFProfileItemProviderAdapterFactory();
-		return new AdapterFactoryLabelProvider(adapterFactory);
-	}
+    private static ILabelProvider getLabelProvider() {
+        final EMFProfileItemProviderAdapterFactory adapterFactory = new EMFProfileItemProviderAdapterFactory();
+        return new AdapterFactoryLabelProvider(adapterFactory);
+    }
 
 }
--- plugins/org.palladiosimulator.editors.gmf.runtime.diagram.ui.extension/src/org/palladiosimulator/editors/gmf/runtime/diagram/ui/extension/action/ApplyUnapplyStereotypeAction.java
+++ plugins/org.palladiosimulator.editors.gmf.runtime.diagram.ui.extension/src/org/palladiosimulator/editors/gmf/runtime/diagram/ui/extension/action/ApplyUnapplyStereotypeAction.java
@@ -10,59 +10,54 @@ import org.eclipse.emf.edit.ui.celleditor.FeatureEditorDialog;
 import org.eclipse.emf.edit.ui.provider.AdapterFactoryLabelProvider;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.viewers.ILabelProvider;
-import org.eclipse.sirius.diagram.DDiagramElement;
 import org.eclipse.sirius.tools.api.ui.IExternalJavaAction;
+import org.eclipse.sirius.viewpoint.DSemanticDecorator;
 import org.eclipse.ui.PlatformUI;
 import org.modelversioning.emfprofile.Stereotype;
 import org.modelversioning.emfprofile.provider.EMFProfileItemProviderAdapterFactory;
 import org.palladiosimulator.mdsdprofiles.api.ProfileAPI;
 import org.palladiosimulator.mdsdprofiles.api.StereotypeAPI;
-import org.palladiosimulator.pcm.core.composition.AssemblyContext;
-import org.palladiosimulator.pcm.core.entity.ComposedProvidingRequiringEntity;
 
 /**
- * A sirius action for selecting and applying a {@link Stereotype} to the
- * selected object.
+ * A sirius action for selecting and applying a {@link Stereotype} to the selected object.
  * 
  * @author Max Schettler
  *
  */
 public class ApplyUnapplyStereotypeAction implements IExternalJavaAction {
 
-	private static final ILabelProvider LABEL_PROVIDER = getLabelProvider();
-	private static final String SELECT_APPLIED_STEREOTYPES_MESSAGE = "Select applied stereotypes";
+    private static final ILabelProvider LABEL_PROVIDER = getLabelProvider();
+    private static final String SELECT_APPLIED_STEREOTYPES_MESSAGE = "Select applied stereotypes";
 
-	@Override
-	public boolean canExecute(Collection<? extends EObject> selections) {
-		if (selections.size() != 1)
-			return false;
-		final EObject selection = ((DDiagramElement) selections.iterator().next()).getTarget();
-		return selection instanceof AssemblyContext || selection instanceof ComposedProvidingRequiringEntity;
-	}
+    @Override
+    public boolean canExecute(final Collection<? extends EObject> selections) {
+        return selections.size() == 1;
+    }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public void execute(Collection<? extends EObject> selections, Map<String, Object> parameters) {
-		final EObject target = ((DDiagramElement) selections.iterator().next()).getTarget();
-		final boolean targetHasProfileApplication = ProfileAPI.hasProfileApplication(target.eResource());
-		
-		final FeatureEditorDialog stereotypeSelectionDialog = new FeatureEditorDialog(
-				PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), LABEL_PROVIDER,
-				target, target.eClass().getEAllStructuralFeatures().get(0).getEType(),
-				targetHasProfileApplication ? StereotypeAPI.getAppliedStereotypes(target) : Collections.EMPTY_LIST,
-				SELECT_APPLIED_STEREOTYPES_MESSAGE,
-				targetHasProfileApplication ? StereotypeAPI.getApplicableStereotypes(target) : Collections.EMPTY_LIST,
-				true, false, false);
+    @SuppressWarnings("unchecked")
+    @Override
+    public void execute(final Collection<? extends EObject> selections, final Map<String, Object> parameters) {
+        final EObject target = ((DSemanticDecorator) selections.iterator().next()).getTarget();
+        final boolean targetHasProfileApplication = ProfileAPI.hasProfileApplication(target.eResource());
 
-		if (stereotypeSelectionDialog.open() == Dialog.OK) {
-			StereotypeAPI.updateStereotypeApplications(target, (EList<Stereotype>) stereotypeSelectionDialog.getResult());
-		}
-		
-	}
+        final FeatureEditorDialog stereotypeSelectionDialog = new FeatureEditorDialog(
+                PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), LABEL_PROVIDER, target,
+                target.eClass().getEAllStructuralFeatures().get(0).getEType(),
+                targetHasProfileApplication ? StereotypeAPI.getAppliedStereotypes(target) : Collections.EMPTY_LIST,
+                SELECT_APPLIED_STEREOTYPES_MESSAGE,
+                targetHasProfileApplication ? StereotypeAPI.getApplicableStereotypes(target) : Collections.EMPTY_LIST,
+                true, false, false);
 
-	private static ILabelProvider getLabelProvider() {
-		final EMFProfileItemProviderAdapterFactory adapterFactory = new EMFProfileItemProviderAdapterFactory();
-		return new AdapterFactoryLabelProvider(adapterFactory);
-	}
+        if (stereotypeSelectionDialog.open() == Dialog.OK) {
+            StereotypeAPI.updateStereotypeApplications(target,
+                    (EList<Stereotype>) stereotypeSelectionDialog.getResult());
+        }
+
+    }
+
+    private static ILabelProvider getLabelProvider() {
+        final EMFProfileItemProviderAdapterFactory adapterFactory = new EMFProfileItemProviderAdapterFactory();
+        return new AdapterFactoryLabelProvider(adapterFactory);
+    }
 
 }
