268ae5412446050b759a32d18729f2f50e6ad6c2
Amine Kechaou <aminekechaou@gmail.com>
2016-05-24 22:26:58 +0200

    Unified plugin files

--- plugins/org.palladiosimulator.editors.repository.externaljavaactions/src/org/palladiosimulator/editors/repository/externaljavaactions/Activator.java
+++ plugins/org.palladiosimulator.editors.repository.externaljavaactions/src/org/palladiosimulator/editors/repository/externaljavaactions/Activator.java
@@ -9,7 +9,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "RepositoryExternalActions"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "org.palladiosimulator.editors.repository.externaljavaactions"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
--- plugins/org.palladiosimulator.editors.repository/src/org/palladiosimulator/editors/repository/Activator.java
+++ plugins/org.palladiosimulator.editors.repository/src/org/palladiosimulator/editors/repository/Activator.java
@@ -13,7 +13,7 @@ import org.osgi.framework.BundleContext;
  */
 public class Activator extends AbstractUIPlugin {
     // The plug-in ID
-    public static final String PLUGIN_ID = "repository.design";
+    public static final String PLUGIN_ID = "org.palladiosimulator.editors.repository";
 
     // The shared instance
     private static Activator plugin;
