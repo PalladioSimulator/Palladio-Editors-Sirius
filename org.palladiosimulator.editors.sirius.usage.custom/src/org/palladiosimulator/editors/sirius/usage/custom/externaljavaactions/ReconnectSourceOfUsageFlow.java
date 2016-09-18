4f78eebdc9c4299aa1fdc35003c15fa5455d74a6
Hannes Kuchelmeister <hannes@kuchelmeister.org>
2016-09-18 17:33:00 +0200

    [Editors-09]usage flow edges can now be reconnected via drag and drop

---
+++ org.palladiosimulator.editors.sirius.usage.custom/src/org/palladiosimulator/editors/sirius/usage/custom/externaljavaactions/ReconnectSourceOfUsageFlow.java
@@ -0,0 +1,31 @@
+package org.palladiosimulator.editors.sirius.usage.custom.externaljavaactions;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.sirius.diagram.business.internal.metamodel.spec.DNodeListSpec;
+import org.eclipse.sirius.tools.api.ui.IExternalJavaAction;
+import org.palladiosimulator.pcm.usagemodel.AbstractUserAction;
+import org.palladiosimulator.pcm.usagemodel.impl.AbstractUserActionImpl;
+public class ReconnectSourceOfUsageFlow implements IExternalJavaAction {
+
+    @Override
+    public boolean canExecute(Collection<? extends EObject> selections) {
+        return true;
+    }
+
+    @Override
+    public void execute(Collection<? extends EObject> selections, Map<String, Object> parameters) {
+
+        AbstractUserAction oldSource = (AbstractUserAction) parameters.get("source");
+        AbstractUserAction newSource = (AbstractUserAction) parameters.get("target");
+
+
+        DNodeListSpec otherEnd = (DNodeListSpec) parameters.get("otherEnd");
+
+        newSource.setSuccessor((AbstractUserActionImpl) otherEnd.getTarget());
+        oldSource.setSuccessor(null);
+    }
+
+}
