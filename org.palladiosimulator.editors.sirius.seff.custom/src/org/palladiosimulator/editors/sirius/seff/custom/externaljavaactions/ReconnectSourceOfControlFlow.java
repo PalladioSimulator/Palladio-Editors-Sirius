e67c4db15e8f35e4d10014c8bbea491c85ea90e2
Hannes Kuchelmeister <hannes@kuchelmeister.org>
2016-09-23 19:20:10 +0200

    [EDITORS-9] reconnection of edges now possible again in usage and seff

--- org.palladiosimulator.editors.sirius.seff.custom/src/org/palladiosimulator/editors/sirius/seff/custom/externaljavaactions/ReconnectSourceOfControlFlow.java
+++ org.palladiosimulator.editors.sirius.seff.custom/src/org/palladiosimulator/editors/sirius/seff/custom/externaljavaactions/ReconnectSourceOfControlFlow.java
@@ -4,8 +4,10 @@ import java.util.Collection;
 import java.util.Map;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.sirius.diagram.business.internal.metamodel.spec.DNodeSpec;
 import org.eclipse.sirius.diagram.impl.DNodeContainerImpl;
 import org.eclipse.sirius.tools.api.ui.IExternalJavaAction;
+import org.eclipse.sirius.viewpoint.DRepresentationElement;
 import org.palladiosimulator.pcm.seff.AbstractInternalControlFlowAction;
 
 public class ReconnectSourceOfControlFlow implements IExternalJavaAction {
@@ -21,7 +23,7 @@ public class ReconnectSourceOfControlFlow implements IExternalJavaAction {
         AbstractInternalControlFlowAction oldSource = (AbstractInternalControlFlowAction) parameters.get("source");
         AbstractInternalControlFlowAction newSource = (AbstractInternalControlFlowAction) parameters.get("target");
 
-        DNodeContainerImpl otherEnd = (DNodeContainerImpl) parameters.get("otherEnd");
+        DRepresentationElement otherEnd = (DRepresentationElement) parameters.get("otherEnd");
 
         newSource.setSuccessor_AbstractAction((AbstractInternalControlFlowAction) otherEnd.getTarget());
         oldSource.setSuccessor_AbstractAction(null);
--- org.palladiosimulator.editors.sirius.usage.custom/src/org/palladiosimulator/editors/sirius/usage/custom/externaljavaactions/ReconnectSourceOfUsageFlow.java
+++ org.palladiosimulator.editors.sirius.usage.custom/src/org/palladiosimulator/editors/sirius/usage/custom/externaljavaactions/ReconnectSourceOfUsageFlow.java
@@ -4,8 +4,10 @@ import java.util.Collection;
 import java.util.Map;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.sirius.diagram.business.internal.metamodel.spec.DNodeContainerSpec;
 import org.eclipse.sirius.diagram.impl.DNodeImpl;
 import org.eclipse.sirius.tools.api.ui.IExternalJavaAction;
+import org.eclipse.sirius.viewpoint.DRepresentationElement;
 import org.palladiosimulator.pcm.usagemodel.AbstractUserAction;
 import org.palladiosimulator.pcm.usagemodel.impl.AbstractUserActionImpl;
 public class ReconnectSourceOfUsageFlow implements IExternalJavaAction {
@@ -16,13 +18,11 @@ public class ReconnectSourceOfUsageFlow implements IExternalJavaAction {
     }
 
     @Override
-    public void execute(Collection<? extends EObject> selections, Map<String, Object> parameters) {
-
+    public void execute(Collection<? extends EObject> selections, Map<String, Object> parameters) {    	
         AbstractUserAction oldSource = (AbstractUserAction) parameters.get("source");
         AbstractUserAction newSource = (AbstractUserAction) parameters.get("target");
-
-
-        DNodeImpl otherEnd = (DNodeImpl) parameters.get("otherEnd");
+        
+        DRepresentationElement otherEnd = (DRepresentationElement) parameters.get("otherEnd");
 
         newSource.setSuccessor((AbstractUserActionImpl) otherEnd.getTarget());
         oldSource.setSuccessor(null);
