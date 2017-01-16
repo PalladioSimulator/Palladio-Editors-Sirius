f22219e956e59fa7181fb33f764391787057f514
Hannes Kuchelmesiter <hannes@kuchelmeister.org>
2017-01-16 19:11:43 +0100

    [Editors-154] used instanceof instead of ugly string manipulation

--- org.palladiosimulator.editors.sirius.services/src/org/palladiosimulator/editors/sirius/services/ComposedProvidingEntityTypeService.java
+++ org.palladiosimulator.editors.sirius.services/src/org/palladiosimulator/editors/sirius/services/ComposedProvidingEntityTypeService.java
@@ -1,17 +1,26 @@
 package org.palladiosimulator.editors.sirius.services;
 
+import org.palladiosimulator.architecturaltemplates.Completion;
 import org.palladiosimulator.pcm.core.entity.ComposedProvidingRequiringEntity;
+import org.palladiosimulator.pcm.repository.CompositeComponent;
+import org.palladiosimulator.pcm.subsystem.SubSystem;
+import org.palladiosimulator.pcm.system.System;
 
 public class ComposedProvidingEntityTypeService {
     public ComposedProvidingEntityTypeService() {
     }
     
     public String getTypeString(ComposedProvidingRequiringEntity entity) {
-        for (int i = 0; i < entity.getClass().getInterfaces().length; i++) {
-            if (entity.getClass().getSimpleName().contains(entity.getClass().getInterfaces()[i].getSimpleName())) {
-                return entity.getClass().getInterfaces()[i].getSimpleName();
-            }
+        if (entity instanceof System) {
+            return "System";
+        } else if (entity instanceof SubSystem) {
+            return "SubSystem";
+        } else if (entity instanceof CompositeComponent) {
+            return "CompositeComponent";
+        } else if (entity instanceof Completion) {
+            return "Completion";
+        } else {
+            return "ComposedProvidingRequiringEntity";
         }
-        return "";
     }
 }
