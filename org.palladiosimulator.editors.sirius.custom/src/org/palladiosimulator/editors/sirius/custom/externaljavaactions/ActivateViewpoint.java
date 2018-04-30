60bdfccc43b4c7ee36c8a1ba52c086a16bc1bbb1
Amine Kechaou <aminekechaou@gmail.com>
2018-04-30 18:12:08 +0200

    EDITORS-217 , EDITORS-218

--- org.palladiosimulator.editors.sirius.custom/src/org/palladiosimulator/editors/sirius/custom/externaljavaactions/ActivateViewpoint.java
+++ org.palladiosimulator.editors.sirius.custom/src/org/palladiosimulator/editors/sirius/custom/externaljavaactions/ActivateViewpoint.java
@@ -10,26 +10,49 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.sirius.business.api.session.Session;
 import org.eclipse.sirius.business.api.session.SessionManager;
 import org.eclipse.sirius.tools.api.ui.IExternalJavaAction;
+import org.eclipse.sirius.viewpoint.description.RepresentationDescription;
+import org.eclipse.sirius.viewpoint.description.Viewpoint;
 import org.palladiosimulator.editors.sirius.custom.util.SiriusCustomUtil;
 
 public class ActivateViewpoint implements IExternalJavaAction {
 
-	@Override
-	public void execute(Collection<? extends EObject> selections, Map<String, Object> parameters) {
-		EObject element = (EObject) parameters.get("element");
-		Session session = SessionManager.INSTANCE.getSession(element);
+    @Override
+    public void execute(Collection<? extends EObject> selections, Map<String, Object> parameters) {
+        EObject element = (EObject) parameters.get("element");
+        Session session = SessionManager.INSTANCE.getSession(element);
 
-		String viewpointName = (String) parameters.get("Viewpoint");
-		
-		List<String> selectedViewpoints = new ArrayList<String>();
-		selectedViewpoints.add(viewpointName);
-		SiriusCustomUtil.selectViewpoints(session, selectedViewpoints, true, new NullProgressMonitor());
+        String viewpointName = (String) parameters.get("Viewpoint");
 
-	}
+        List<String> selectedViewpoints = new ArrayList<String>();
+        selectedViewpoints.add(viewpointName);
 
-	@Override
-	public boolean canExecute(Collection<? extends EObject> selections) {
-		return true;
-	}
+        if (parameters.containsKey("Diagram Name") && parameters.containsKey("RepresentationDescription")) {
+            SiriusCustomUtil.selectViewpoints(session, selectedViewpoints, false, new NullProgressMonitor());
+            String representationDescriptionName = (String) parameters.get("RepresentationDescription");
+            String diagramName = (String) parameters.get("Diagram Name");
+
+            Viewpoint viewpoint = SiriusCustomUtil.getSelectedViewpointByName(session, viewpointName);
+            RepresentationDescription description = SiriusCustomUtil.findDescription(viewpoint,
+                    representationDescriptionName);
+            String representationName = null;
+            if (diagramName != null && !diagramName.isEmpty()) {
+                representationName = diagramName;
+            } else {
+                representationName = ((description.getLabel() == null) ? description.getName()
+                        : description.getLabel());
+            }
+            SiriusCustomUtil.createRepresentation(session, representationName, description, element,
+                    new NullProgressMonitor());
+
+        } else {
+            SiriusCustomUtil.selectViewpoints(session, selectedViewpoints, true, new NullProgressMonitor());
+        }
+
+    }
+
+    @Override
+    public boolean canExecute(Collection<? extends EObject> selections) {
+        return true;
+    }
 
 }
