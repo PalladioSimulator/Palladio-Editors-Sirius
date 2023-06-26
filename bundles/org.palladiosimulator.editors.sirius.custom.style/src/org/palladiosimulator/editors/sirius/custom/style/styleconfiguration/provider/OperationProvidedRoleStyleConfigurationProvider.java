920ad893d18032eec83e20042ee97a541ec80c82
SebastianWeberFZI <sebastian.weber@fzi.de>
2023-06-26 13:01:33 +0200

    Remove usage of PixelOffsetDecorator

--- bundles/org.palladiosimulator.editors.sirius.custom.style/src/org/palladiosimulator/editors/sirius/custom/style/styleconfiguration/provider/OperationProvidedRoleStyleConfigurationProvider.java
+++ bundles/org.palladiosimulator.editors.sirius.custom.style/src/org/palladiosimulator/editors/sirius/custom/style/styleconfiguration/provider/OperationProvidedRoleStyleConfigurationProvider.java
@@ -18,7 +18,7 @@ public class OperationProvidedRoleStyleConfigurationProvider implements IStyleCo
 
     @Override
     public StyleConfiguration createStyleConfiguration(final DiagramElementMapping mapping, final Style style) {
-        return new ConnectorStyleConfiguration(new PixelOffsetDecorator(new OrientedHalfAlphaBasedAnchorProvider()));
+        return new ConnectorStyleConfiguration(new OrientedHalfAlphaBasedAnchorProvider());
     }
 
     @Override
